80ba49b4295e97be9440b01e175d17a06272e001
atlee19 <atleegraham16@gmail.com>
2020-10-29 11:29:34 -0400

    commented out base route

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
   
-})
+// })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server started at http://localhost:${port}`)
 })
