0ff3e1b0ec742d928b35515d5e150fabdc3943fb
atlee19 <atleegraham16@gmail.com>
2020-11-02 12:38:21 -0500

    got text data to come through on backend

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { env } = require('process')
 const port = 3000
 
+//Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+//Auto sends index.html
 app.use(express.static(path.join(__dirname,'public')))
 
 
@@ -12,7 +18,7 @@ app.post('/', function (req, res) {
 
     //we'll call an object that fetches the data from reddit
 
-    //then it will that object will make a call to the stats engine which
+    //then within in that object will make a call to the stats engine which
     //will then return a result 
 
     //we'll send back an html page or a PUG with the analysis from the stats engine
