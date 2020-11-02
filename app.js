0e3e320ba6f7d0201adb7d9023a08642c31579e6
atlee19 <atleegraham16@gmail.com>
2020-11-02 11:01:08 -0500

    post request setup

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
 
-// app.get('/', (req, res) => {
-  
-// })
+
+app.post('/', function (req, res) {
+    console.log(req)
+    //we'll get the search query here
+
+    //we'll call an object that fetches the data from reddit
+
+    //then it will that object will make a call to the stats engine which
+    //will then return a result 
+
+    //we'll send back an html page or a PUG with the analysis from the stats engine
+})
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
