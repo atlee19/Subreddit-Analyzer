27429604b04a660e1ba68ad695d31435ab15a141
atlee19 <atleegraham16@gmail.com>
2020-11-05 10:16:06 -0500

    setting up search function and installed axios

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const { env } = require('process')
+const search = require('./lib/search')
 const port = 3000
 
 //Body parser middleware
@@ -13,15 +14,17 @@ app.use(express.static(path.join(__dirname,'public')))
 
 
 app.post('/', function (req, res) {
-    console.log(req.body)
-    //we'll get the search query here
+  //we'll get the search query here
+  const query = req.body.search_field
+  console.log(query)
+    
+  //we'll call an object that fetches the data from reddit
+  search.subreddit(query)
 
-    //we'll call an object that fetches the data from reddit
+  //then within in that object will make a call to the stats engine which
+  //will then return a result 
 
-    //then within in that object will make a call to the stats engine which
-    //will then return a result 
-
-    //we'll send back an html page or a PUG with the analysis from the stats engine
+  //we'll send back an html page or a PUG with the analysis from the stats engine
 })
 
 app.listen(port, () => {
